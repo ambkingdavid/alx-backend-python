b20be4f0331f765830fd077b8aef9cba94b602f7
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-03 10:23:51 +0100

    add task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a module that defines a sum_list function"""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of the input_list"""
+    return sum(input_list)
