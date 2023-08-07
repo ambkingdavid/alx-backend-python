27acc92eba6c1a6a90cfbea5a454235bcbffcd29
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-07 14:13:54 +0100

    typo error fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """defines a wait_n function that returns a list of delays"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
