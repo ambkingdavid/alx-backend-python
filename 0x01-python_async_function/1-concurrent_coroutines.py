ccf91975b3efd8001fa9c4feba117fbe1fba9b96
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-07 14:13:17 +0100

    import typing module

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """a module that contains the wait_n func"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """defines a wait_n function that returns a list of delays"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = []
