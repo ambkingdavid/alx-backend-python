e050afdd80d3cdd32b7444ab63e2e1a326b182e9
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-08 10:21:31 +0100

    initial commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""a module that contains the async_generator function"""
+
+import asyncio
+import random
+
+async def async_generator():
+    """an Async generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
