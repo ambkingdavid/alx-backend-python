dad0d9c3325debfb1247d43d7db522a7a0fc09b0
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-09 14:31:56 +0100

    add anotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,11 @@
 
 import asyncio
 import random
+from types import NoneType
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None] :
+async def async_generator() -> Generator[float, NoneType, NoneType] :
     """an Async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
