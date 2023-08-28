9460a2612784bf46a96904378d4b57ad8a659295
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-28 10:06:39 +0100

    add tasks

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,10 @@
 
 import unittest
 from utils import access_nested_map as access
+from utils import get_json, memoize
 from parameterized import parameterized
+from unittest.mock import Mock, patch
+from typing import Any, Dict, Mapping, Sequence
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +33,45 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(output):
             result = access(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url: str, payload: Dict):
+        """
+        TestCase for get_json method
+        """
+        with patch('utils.requests.get') as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = payload
+            mock_get.return_value = mock_response
+            result = get_json(url)
+            mock_get.assert_called_once_with(url)
+            self.assertEqual(result, payload)
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test memoize
+    """
+    def test_memoize(self):
+        """
+        TestCase for utils.memoize
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method', return_value=42) as pmock:
+            test_obj = TestClass()
+            self.assertEqual(test_obj.a_property, 42)
+            self.assertEqual(test_obj.a_property, 42)
+            pmock.assert_called_once()
+
 
 
 if __name__ == '__main__':
