71a2e464ab54fa463202dcc8a412e724f4bc0200
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-28 08:53:43 +0100

    add new dir

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Test module"""
+
+import unittest
+from utils import access_nested_map as access
+from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test utils methods
+    """
+    @parameterized.expand([
+        ({"a": 1}, ["a"], 1),
+        ({"a": {"b": 2}}, ["a"], {"b": 2}),
+        ({"a": {"b": 2}}, ["a", "b"], 2),
+    ])
+    def test_access_nested_map(self, data, path, expected_result):
+        """Test access_nested_map method"""
+        result = access(data, path)
+        self.assertEqual(result, expected_result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
