ddc7e0599cd832f3606b81fe9bbdb7de4e9feb5d
ambkingdavid <kelvinwooldbridge@gmail.com>
2025-09-16 16:36:28 +0100

    add role to the user serializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,6 +3,7 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer[User]):
+    role = serializers.CharField(read_only=True)
     class Meta: # type: ignore
         model = User
         fields = [
